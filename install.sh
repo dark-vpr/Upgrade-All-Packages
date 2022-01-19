ab37befb540ec37cf36ab76bfb60e7c70220d5d4
darkvpier <shraidshebin@gmail.com>
2022-01-19 14:24:41 -0500

    git broken

---
+++ install.sh
@@ -0,0 +1,58 @@
+#/bin/bash
+if [ -e /bin/UGpack ]; then
+    echo
+    echo "UGpack has already been Installed....."
+    while true; do
+        echo
+        read -p "Do you replace existing Package..... [Y|n]: " yn
+        case $yn in
+        [nN]*)
+            echo "Aborting....."
+            break
+            ;;
+        [yY]*)
+            sudo ln -sf $PWD/UGpack /bin/UGpack
+            echo
+            echo "Installation Completed.... (To Run Enter \"sudo UGpack\")"
+            break
+            ;;
+        *)
+            echo 'Wrong Choice.....'
+            ;;
+        esac
+    done
+else
+    sudo ln -sf $PWD/UGpack /bin/UGpack
+    echo
+    echo "Installation Completed.... (To Run Enter \"sudo UGpack\")"
+fi
+
+if [ -e /opt/ugpack.conf ]; then
+    echo
+    echo
+    echo "Current Git Paths:"
+    while read LINE; do
+        if echo "$LINE" | grep -q "#"; then
+            continue
+        else
+            echo $LINE
+        fi
+    done </opt/ugpack.conf
+    echo
+    while true; do
+        echo
+        read -p "Enter the Path to your Git Packages (if none, Press ctrl + c): " gp
+        sudo echo $gp >>/opt/ugpack.conf
+    done
+else
+    echo
+    sudo touch /opt/ugpack.conf
+    sudo chmod 666 /opt/ugpack.conf
+    sudo echo "# Add the Path of your Git Packages" >/opt/ugpack.conf
+    sudo echo >>/opt/ugpack.conf
+    while true; do
+        echo
+        read -p "Enter The Path to your Git Package (if none, press ctrl + c): " gp
+        sudo echo $gp >>/opt/ugpack.conf
+    done
+fi
