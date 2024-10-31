51b92ce0c36f8831293386c73c6c062beb2b47df
dark-vpr <84262682+dark-vpr@users.noreply.github.com>
2024-10-31 16:59:06 +0000

    Refactor UGpack and installation scripts for improved syntax and add Rustup and Cargo upgrade support

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [ "$OSTYPE" = "linux-android" ]; then
-	exec $PWD/.package/termux-install.sh
+	exec "$PWD"/.package/termux-install.sh
 elif [ "$OSTYPE" = "linux-gnu" ]; then
-	exec $PWD/.package/debian-install.sh
+	exec "$PWD"/.package/debian-install.sh
 else
 	echo "Unsupported OS type: $OSTYPE"
 	exit 1
